224dc8e558b53e01ced6849b1a8666af635f6e03
Benedikt Sauer <filmor@gmail.com>
2008-05-01 01:31:37 +0200

    Updated some tests with newer interfaces.
    Still todo: Tests for a whole bunch of classes.
    There should be a test case for /every/ non-utility class.

--- test/lyrics.cpp
+++ test/lyrics.cpp
@@ -1,19 +1,20 @@
-#include "song_info.hpp"
+#include "song.hpp"
 
 #include <iostream>
 
 int main (int argc, char** argv)
 {
+    Py_Initialize ();
     /// Currently not working, needs Python
     if (argc < 2)
         throw 1;
 
     std::cout << argv[1] << std::endl;
-    star::song_info f (argv[1]);
+    star::song f (argv[1]);
     
-    star::song_info::lines l = f.get_lines ();
+    star::song::lines l = f.get_lines ();
     
-    for (star::song_info::lines::const_iterator i = l.begin (); i != l.end ();
+    for (star::song::lines::const_iterator i = l.begin (); i != l.end ();
             ++i)
         std::cout << i->get<0> () <<  ' ' << i->get<1> () << std::endl;
 }
